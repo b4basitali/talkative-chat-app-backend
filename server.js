e9b3788194a4b20665d305663feac2a605a2cb36
mdazlaanzubair <mdazlaan1996@gmail.com>
2023-12-09 20:16:47 +0500

    cors origin error fixed

--- server.js
+++ server.js
@@ -39,7 +39,10 @@ const server = app.listen(
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "http://localhost:3000",
+    origin: [
+      "http://localhost:3000",
+      "https://talkative-chat-app-api.vercel.app",
+    ],
     // credentials: true,
   },
 });
