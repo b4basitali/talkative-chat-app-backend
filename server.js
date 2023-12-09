efb7e3eb3da79ec03e3449f95c8485febe8f8778
mdazlaanzubair <mdazlaan1996@gmail.com>
2023-12-09 22:11:36 +0500

    endpoint changed

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ const server = app.listen(
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "https://talkative-chat-app.vercel.app",
+    origin: "https://talkative-chatapp.vercel.app",
     // credentials: true,
   },
 });
